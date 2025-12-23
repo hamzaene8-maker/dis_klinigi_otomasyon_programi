cfed8999da905e87d414d9c834ff69c95cb21a38
GAME IN GAME <GAME IN GAME@GAMINGPC>
2025-12-23 17:49:32 +0300

    Proje dosyası ekle.

---
+++ dis_klinigi_otomasyon_programi/Form1.cs
@@ -0,0 +1,10 @@
+namespace dis_klinigi_otomasyon_programi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
